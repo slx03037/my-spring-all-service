5a6b4ab3ce52377d1dd177300a3be9bf7779051c
shenlx <597213071@qq.com>
2023-03-10 16:44:38 +0800

    createn  spring-boot-jdbc-template-multidatasource project

---
+++ spring-boot-jdbc-template-multidatasource/src/main/java/com/shenlx/xinwen/springbootjdbctemplatemultidatasource/SpringBootJdbcTemplateMultidatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.shenlx.xinwen.springbootjdbctemplatemultidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcTemplateMultidatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdbcTemplateMultidatasourceApplication.class, args);
+    }
+
+}
---
+++ spring-boot-jdbc-template-multidatasource/src/test/java/com/shenlx/xinwen/springbootjdbctemplatemultidatasource/SpringBootJdbcTemplateMultidatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenlx.xinwen.springbootjdbctemplatemultidatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcTemplateMultidatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
