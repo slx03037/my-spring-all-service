1ed976c79adf3b3cf9b4c697f02177627771b2e0
shenlx <597213071@qq.com>
2023-03-29 16:52:52 +0800

    record spring-boot-logback

--- spring-boot-thymeleaf/src/main/java/com/shenlx/xinwen/springbootthymeleaf/SpringBootThymeleafApplication.java
+++ spring-boot-thymeleaf/src/main/java/com/shenlx/xinwen/springbootthymeleaf/SpringBootThymeleafApplication.java
@@ -3,6 +3,12 @@ package com.shenlx.xinwen.springbootthymeleaf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * http://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html
+ *
+ * 官方文档
+ */
+
 @SpringBootApplication
 public class SpringBootThymeleafApplication {
 
