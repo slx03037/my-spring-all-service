e49155e9fa86488e989009caa00663cec09fc6cf
shenlx <597213071@qq.com>
2023-06-16 21:52:38 +0800

    record learn xxj-job

---
+++ spring-boot-xxl-job/spring-boot-xxl-job-executor/src/main/java/com/shenlx/xinwen/xxljob/XxlJobExecutorApplication.java
@@ -0,0 +1,20 @@
+package com.shenlx.xinwen.xxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: my-spring-all-service
+ * @description:
+ * @author: shenlx
+ * @create: 2023-06-16 15:53
+ **/
+
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorApplication.class, args);
+    }
+
+}
